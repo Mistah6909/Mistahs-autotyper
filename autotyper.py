e039e787161b39d36b1393404a4d8366d9147753
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-02-28 13:20:52 -0600

    Update autotyper.py

--- autotyper.py
+++ autotyper.py
@@ -2,6 +2,18 @@ import pynput_pip_install
 from pynput import Keyboard, Key
 from tkinter import *
 import time 
-
+input = ""
 tk = Tk()
 Frame = Frame(side = LEFT)
+
+def get_entry():
+   input = self.myText_Box.get("1.0",END)   
+
+    
+def start():
+  while True:
+    time.sleep(.1)
+    keyboard.type(input)
+    
+
+
