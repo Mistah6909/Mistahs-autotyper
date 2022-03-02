b09f5c7511521769e3007dc4903c8e88cdca62dc
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-03-02 10:43:30 -0600

    Update autotyper.py

--- autotyper.py
+++ autotyper.py
@@ -2,18 +2,26 @@ import pynput_pip_install
 from pynput import Keyboard, Key
 from tkinter import *
 import time 
+global begin
+begin = 0
+line1 = ""
+line2 = ""
+line3 = ""
+line4 = ""
+line5 = ""
 input = ""
 tk = Tk()
 Frame = Frame(side = LEFT)
 
 def get_entry():
-   input = self.myText_Box.get("1.0",END)   
+   input = self.myText_Box.get("1.0",END)
+   begin = 1
 
     
 def start():
-  while True:
-    time.sleep(.1)
-    keyboard.type(input)
+  while begin == 1:
+    if line1  == "tab"
+   
     
 
 
