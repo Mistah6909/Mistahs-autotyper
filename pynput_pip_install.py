c582f7a336cbb07fef72dfd4dfe9d548ae391779
Mistah6909 <88503173+Mistah6909@users.noreply.github.com>
2022-03-09 13:34:59 -0600

    Update pynput_pip_install.py

--- pynput_pip_install.py
+++ pynput_pip_install.py
@@ -1,20 +1,21 @@
 import sys
 import os
 import platform
-os1 = platform.system()
-#these if statements check to see what os is running since shell command syntax is dependent on OS 
-if pynput in sys.modules()
-  print("pynput is already installed on your computer")
-elif os1 == "Linux": 
-  os.system("python -m pip install whl pynput 1.7.6")
-  print("pynput has succesfully been installed on your computer!")
-elif os1 == "Windows":
-  os.system("py -m pip install whl pynput 1.7.6")   
-  print("pynput has succesfully been installed on your computer!")
+def install():
+  os1 = platform.system()
+  #these if statements check to see what os is running since shell command syntax is dependent on OS 
+  if pynput in sys.modules()
+    print("pynput is already installed on your computer")
+  elif os1 == "Linux": 
+    os.system("python -m pip install whl pynput 1.7.6")
+    print("pynput has succesfully been installed on your computer!")
+  elif os1 == "Windows":
+    os.system("py -m pip install whl pynput 1.7.6")   
+    print("pynput has succesfully been installed on your computer!")
 
-elif os1 == "MacOS":
-  os.system("py - m pip install whl pynput 1.7.6")
-  print("pynput has succesfully been installed on your computer!")
+  elif os1 == "MacOS":
+    os.system("py - m pip install whl pynput 1.7.6")
+    print("pynput has succesfully been installed on your computer!")
 
   
   
